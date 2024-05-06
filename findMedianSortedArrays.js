1d61af1dcd0839bcb780e8c38e312e248a871985
arti chandane <aaratichandane@gmail.com>
2024-05-07 00:59:32 +0530

    Find median sorted array

---
+++ findMedianSortedArrays.js
@@ -0,0 +1,14 @@
+function findMedianSortedArrays(nums1, nums2) {
+    
+};
+
+function mergeUnsortedToSortedArray(nums1, nums2){
+    while(nums1.length){
+
+    }
+}
+
+
+
+const nums1 = [1,3], nums2 = [2];
+console.log(findMedianSortedArrays(nums1, nums2));
