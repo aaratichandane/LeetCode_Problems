579a6d66abe69bbeb65f3eda3500378e9fd8b561
arti chandane <aaratichandane@gmail.com>
2024-05-10 16:35:23 +0000

    Commit for day 51

---
+++ deleteDuplicates.js
@@ -0,0 +1,24 @@
+class ListNode {
+    constructor(val, next = null) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+  
+  function deleteDuplicates(head) {
+    let current = head;
+  
+    while (current !== null && current.next !== null) {
+      if (current.val === current.next.val) {
+        current.next = current.next.next;
+      } else {
+        current = current.next;
+      }
+    }
+  
+    return head;
+  }
+
+  const list = new ListNode(1, new ListNode(1, new ListNode(2)));
+  const result = deleteDuplicates(list);
+  console.log(result);                                                     
