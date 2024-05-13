f7e0c9248c6afbe5b37117f99c0b4a69d5167bb3
arti chandane <aaratichandane@gmail.com>
2024-05-14 01:02:37 +0530

    max subarray

---
+++ maxSubarray.js
@@ -0,0 +1,11 @@
+var maxSubArray = function(nums) {
+    let subArray = [], isUnique = true;
+
+    for(let i=0; i<= nums.length){
+
+    }
+
+};
+
+const nums = [-2,1,-3,4,-1,2,1,-5,4]
+console.log(maxSubArray(nums))
