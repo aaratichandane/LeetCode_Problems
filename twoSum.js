23a3490d163aeb85bd69dab17e62e3373b9d0dc2
arti chandane <aaratichandane@gmail.com>
2024-05-23 01:08:28 +0530

    Two Sum

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+var twoSum = function (nums, target){
+    var n = nums.length;
+    var map = new Map();
+
+    for(let i =0; i<n; i++){
+        var remaining = target - nums[i];
+        if(map.has(remaining)){
+           return map.get(remaining)
+        }
+        map.set(i,remaining);
+    }
+    
+    return;
+}
+
+const nums = [2,7,11,15], target = 9;
+console.log(twoSum(nums, target))
